23bfa0466cd76b76aac9e109e129ee89b403b844
clockwerkz <clockwerkz@gmail.com>
2019-08-22 21:32:28 -0700

    adds minEdits challenge

--- javascript/dynamic_probs/minEdits.js
+++ javascript/dynamic_probs/minEdits.js
@@ -9,7 +9,10 @@
 // All of the above operations are of equal cost.
 
 function minEdits(str1, str2, ptr1=0, ptr2=0) {
-    if (ptr1 === str1.length && ptr2 === str2.length) return 0;
+    if (ptr2 >= str2.length-1) return 0;
+    if (ptr1 === str1.length-1) {
+        return 1 + minEdits(str1, str2, ptr1, ptr2+1)
+    } 
     if (ptr1 ===str1.length-1 && ptr2===str2.length-1) {
         return str1[ptr1] === str2[ptr2] ? 0 : 1
     }
@@ -19,15 +22,16 @@ function minEdits(str1, str2, ptr1=0, ptr2=0) {
     if (str1[ptr1] === str2[ptr2]) {
         return 0 + minEdits(str1, str2, ptr1+1, ptr2+1);
     } else {
-        ptr1  = (ptr1 === str1.length) ? str1.length : ptr1 + 1;
-        let replace = 1 + minEdits(str1, str2, ptr1, ptr2+1);
+        let replace = 1 + minEdits(str1, str2, ptr1+1, ptr2+1);
         let insert = 1 + minEdits(str1, str2, ptr1, ptr2+1);
         return replace < insert ? replace : insert;
     }
 }
 
-console.log(minEdits('a','a')); //0
-console.log(minEdits('a','b')); //1
-console.log(minEdits('a','baa')); //3
-console.log(minEdits('cat','cut')) //1
-console.log(minEdits('sunday','saturday')) //3
+console.log(minEdits('a','a'), '0'); //0
+console.log(minEdits('a','b'), '1'); //1
+console.log(minEdits('a','baa'), '3'); //3
+console.log(minEdits('cat','cut'),'1') //1
+console.log(minEdits('sunday','saturday'), '3') //3
+console.log(minEdits('geek','gesk'), '1'); //1
+console.log(minEdits('gek','gekeee'), '3'); //3
