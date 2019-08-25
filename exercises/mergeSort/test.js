6d95241eccf555fe9db86fe7ab2bc31c699b43a8
clockwerkz <clockwerkz@gmail.com>
2019-08-24 17:35:00 -0700

    test misspelling

--- exercises/mergeSort/test.js
+++ exercises/mergeSort/test.js
@@ -12,5 +12,5 @@ test('Merge Sort returns a sorted array', ()=> {
 
 test('Merge Sort returns another sorted array', ()=> {
     const sortedArray = mergeSort([5,2,9,1,8,23,12,3,56,90]);
-    expect(sortedArray).tpEqual([1,2,3,5,8,9,12,23,56,90]);
+    expect(sortedArray).toEqual([1,2,3,5,8,9,12,23,56,90]);
 });
